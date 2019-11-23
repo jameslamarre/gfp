118972b462282b5f7f50efe746aeb825029793f9
James La Marre <jameslamarre@gmail.com>
2019-11-23 14:14:31 -0500

    first pass at templating, header/footer, styling bump

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,29 @@
 class App {
 
 	constructor() {
+		this.header = document.querySelector('.header');
+    this.body = document.querySelector('body');
+
+    this.content = document.querySelector('.content-wrapper');
+
+		this.footer = document.querySelector('.footer-wrapper')
+		this.loader = document.querySelector('.loader');
+
 		this.bindGlobalEvents();
 	}
 
 	bindGlobalEvents() {
+		window.addEventListener('load', this.turnOffLoader.bind(this));
 
+	}
+
+	turnOffLoader() {
+		this.loader.parentNode.removeChild(this.loader);
+		this.content.style.display = 'block';
+
+		if (this.footer) {
+			this.footer.classList.add('show');
+		}
 	}
 }
 
