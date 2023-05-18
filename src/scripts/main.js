15ebcd5d65ea2c5a4ca1e011d868b88023e2b7f2
James La Marre <jameslamarre@gmail.com>
2023-05-18 13:17:52 -0700

    feat: upgrade u

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,98 +1,102 @@
 class App {
-
 	constructor() {
-		this.header = document.querySelector('.header');
-    this.body = document.querySelector('body');
+		this.header = document.querySelector(".header");
+		this.body = document.querySelector("body");
 
-		this.blobs = document.querySelectorAll('.blob');
+		this.blobs = document.querySelectorAll(".blob");
 
-		this.nav = document.querySelector('.header-right__nav');
+		this.nav = document.querySelector(".header-right__nav");
 
-		this.mobileMenu = document.querySelector('.header-mobile');
-		this.mobileMenuBtn = document.querySelector('.header-mobile__button');
+		this.mobileMenu = document.querySelector(".header-mobile");
+		this.mobileMenuBtn = document.querySelector(".header-mobile__button");
 
-		this.mobileMenuLists = document.querySelectorAll('.header-mobile__nav .nav-link.primary');
-		this.mobileMenuGroups = document.querySelectorAll('.header-mobile__nav .nav-link-group');
+		this.mobileMenuLists = document.querySelectorAll(
+			".header-mobile__nav .nav-link.primary"
+		);
+		this.mobileMenuGroups = document.querySelectorAll(
+			".header-mobile__nav .nav-link-group"
+		);
 
-    this.content = document.querySelector('.content-wrapper');
+		this.content = document.querySelector(".content-wrapper");
 
-		this.videoLockups = document.querySelectorAll('.content-lockup.video')
-		this.videoCopys = document.querySelectorAll('.content-lockup__copy')
+		this.videoLockups = document.querySelectorAll(".content-lockup.video");
+		this.videoCopys = document.querySelectorAll(".content-lockup__copy");
 
-		this.footer = document.querySelector('.footer-wrapper')
-		this.loader = document.querySelector('.loader');
+		this.footer = document.querySelector(".footer-wrapper");
+		this.loader = document.querySelector(".loader");
 
 		this.bindGlobalEvents();
 	}
 
 	bindGlobalEvents() {
-		window.addEventListener('load', this.turnOffLoader.bind(this));
+		window.addEventListener("load", this.turnOffLoader.bind(this));
 
 		if (window.innerWidth > 850) {
-			if (window.location.pathname == '/') {
+			if (window.location.pathname == "/") {
 				this.content.style.marginTop = this.header.offsetHeight - 1 + "px";
 			} else {
 				this.content.style.marginTop = this.header.offsetHeight + 70 + "px";
 			}
 
-			window.addEventListener('scroll', this.trackScroll.bind(this));
-			document.addEventListener('mousemove', this.mouseListener.bind(this));
+			window.addEventListener("scroll", this.trackScroll.bind(this));
+			document.addEventListener("mousemove", this.mouseListener.bind(this));
 		}
 
-		this.mobileMenuBtn.addEventListener('click', this.toggleMenu.bind(this));
+		this.mobileMenuBtn.addEventListener("click", this.toggleMenu.bind(this));
 
 		for (const list of this.mobileMenuLists) {
-			list.addEventListener('click', this.toggleMobileGroups.bind(this));
+			list.addEventListener("click", this.toggleMobileGroups.bind(this));
 		}
 
 		for (const lockup of this.videoLockups) {
-			lockup.addEventListener('click', this.toggleVideoCopy.bind(this));
+			lockup.addEventListener("click", this.toggleVideoCopy.bind(this));
 		}
 	}
 
 	turnOffLoader() {
 		this.loader.parentNode.removeChild(this.loader);
-		this.content.style.display = 'block';
+		this.content.style.display = "block";
 
 		for (let i = 0; i < this.blobs.length; i++) {
-			this.blobs[i].style.opacity = '0.55';
+			this.blobs[i].style.opacity = "0.55";
 		}
 
 		if (this.footer) {
-			this.footer.classList.add('show');
+			this.footer.classList.add("show");
 		}
 	}
 
 	toggleMenu() {
-    this.body.classList.toggle('no-scroll');
-    this.mobileMenuBtn.classList.toggle('active');
-    this.mobileMenu.classList.toggle('active');
-  }
+		this.body.classList.toggle("no-scroll");
+		this.mobileMenuBtn.classList.toggle("active");
+		this.mobileMenu.classList.toggle("active");
+	}
 
 	toggleMobileGroups(e) {
-		if (e.target.parentNode.classList.contains('active')) {
-			e.target.parentNode.classList.remove('active');
+		if (e.target.parentNode.classList.contains("active")) {
+			e.target.parentNode.classList.remove("active");
 		} else {
-			e.target.parentNode.classList.add('active');
+			e.target.parentNode.classList.add("active");
 		}
 	}
 
 	trackScroll() {
+		console.log("trackScroll");
 		let offset = window.pageYOffset;
 
 		if (offset > 150) {
-			this.header.classList.add('scroll');
+			this.header.classList.add("scroll");
 		} else {
-			this.header.classList.remove('scroll')
+			this.header.classList.remove("scroll");
 		}
 	}
 
 	toggleVideoCopy(e) {
-		const id = e.target.getAttribute('data-id');
+		const id = e.target.getAttribute("data-id");
 
 		this.videoCopys.forEach(function (copy, index) {
-			if (copy.getAttribute('data-id') == id) {
-				copy.classList.add('hidden');
+			if (copy.getAttribute("data-id") == id) {
+				copy.classList.add("hidden");
 			}
 		});
 	}
@@ -100,16 +104,51 @@ class App {
 	mouseListener(e) {
 		let xFactor = e.clientX * 0.05;
 
-		this.blobs[0].style.transform = 'translate3d(' + parseInt(xFactor * 0.4) + 'px, ' + parseInt(xFactor * -1.5) + 'px, 0)';
-		this.blobs[1].style.transform = 'translate3d(' + parseInt(xFactor * -2) + 'px, ' + parseInt(xFactor * -1.5) + 'px, 0)';
+		this.blobs[0].style.transform =
+			"translate3d(" +
+			parseInt(xFactor * 0.4) +
+			"px, " +
+			parseInt(xFactor * -1.5) +
+			"px, 0)";
+		this.blobs[1].style.transform =
+			"translate3d(" +
+			parseInt(xFactor * -2) +
+			"px, " +
+			parseInt(xFactor * -1.5) +
+			"px, 0)";
 
-		this.blobs[2].style.transform = 'translate3d(' + parseInt(xFactor * -0.4) + 'px, ' + parseInt(xFactor) + 'px, 0)';
-		this.blobs[3].style.transform = 'translate3d(' + parseInt(xFactor * -0.6) + 'px, ' + parseInt(xFactor * 0.3) + 'px, 0)';
+		this.blobs[2].style.transform =
+			"translate3d(" +
+			parseInt(xFactor * -0.4) +
+			"px, " +
+			parseInt(xFactor) +
+			"px, 0)";
+		this.blobs[3].style.transform =
+			"translate3d(" +
+			parseInt(xFactor * -0.6) +
+			"px, " +
+			parseInt(xFactor * 0.3) +
+			"px, 0)";
 
-		this.blobs[4].style.transform = 'translate3d(' + parseInt(xFactor * 1.6) + 'px, ' + parseInt(xFactor * -1.2) + 'px, 0)';
-		this.blobs[5].style.transform = 'translate3d(' + parseInt(xFactor * 2) + 'px, ' + parseInt(xFactor * -1) + 'px, 0)';
+		this.blobs[4].style.transform =
+			"translate3d(" +
+			parseInt(xFactor * 1.6) +
+			"px, " +
+			parseInt(xFactor * -1.2) +
+			"px, 0)";
+		this.blobs[5].style.transform =
+			"translate3d(" +
+			parseInt(xFactor * 2) +
+			"px, " +
+			parseInt(xFactor * -1) +
+			"px, 0)";
 
-		this.blobs[6].style.transform = 'translate3d(' + parseInt(xFactor * 1.2) + 'px, ' + parseInt(xFactor * 0.5) + 'px, 0)';
+		this.blobs[6].style.transform =
+			"translate3d(" +
+			parseInt(xFactor * 1.2) +
+			"px, " +
+			parseInt(xFactor * 0.5) +
+			"px, 0)";
 	}
 }
 
--- web/js/bundle.js
+++ web/js/bundle.js
@@ -7,6 +7,9 @@
 /***/ (() => {
 
 function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
@@ -15,67 +18,120 @@ function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input ==
 var App = /*#__PURE__*/function () {
   function App() {
     _classCallCheck(this, App);
-    this.header = document.querySelector('.header');
-    this.body = document.querySelector('body');
-    this.blobs = document.querySelectorAll('.blob');
-    this.mobileMenu = document.querySelector('.header-mobile');
-    this.mobileMenuBtn = document.querySelector('.header-mobile__button');
-    this.content = document.querySelector('.content-wrapper');
-    this.footer = document.querySelector('.footer-wrapper');
-    this.loader = document.querySelector('.loader');
+    this.header = document.querySelector(".header");
+    this.body = document.querySelector("body");
+    this.blobs = document.querySelectorAll(".blob");
+    this.nav = document.querySelector(".header-right__nav");
+    this.mobileMenu = document.querySelector(".header-mobile");
+    this.mobileMenuBtn = document.querySelector(".header-mobile__button");
+    this.mobileMenuLists = document.querySelectorAll(".header-mobile__nav .nav-link.primary");
+    this.mobileMenuGroups = document.querySelectorAll(".header-mobile__nav .nav-link-group");
+    this.content = document.querySelector(".content-wrapper");
+    this.videoLockups = document.querySelectorAll(".content-lockup.video");
+    this.videoCopys = document.querySelectorAll(".content-lockup__copy");
+    this.footer = document.querySelector(".footer-wrapper");
+    this.loader = document.querySelector(".loader");
     this.bindGlobalEvents();
   }
   _createClass(App, [{
     key: "bindGlobalEvents",
     value: function bindGlobalEvents() {
-      window.addEventListener('load', this.turnOffLoader.bind(this));
-      // window.addEventListener('scroll', this.trackScroll.bind(this));
-
-      if (window.innerWidth > 1024) {
-        document.addEventListener('mousemove', this.mouseListener.bind(this));
+      window.addEventListener("load", this.turnOffLoader.bind(this));
+      if (window.innerWidth > 850) {
+        if (window.location.pathname == "/") {
+          this.content.style.marginTop = this.header.offsetHeight - 1 + "px";
+        } else {
+          this.content.style.marginTop = this.header.offsetHeight + 70 + "px";
+        }
+        window.addEventListener("scroll", this.trackScroll.bind(this));
+        document.addEventListener("mousemove", this.mouseListener.bind(this));
       }
-      this.mobileMenuBtn.addEventListener('click', this.toggleMenu.bind(this));
+      this.mobileMenuBtn.addEventListener("click", this.toggleMenu.bind(this));
+      var _iterator = _createForOfIteratorHelper(this.mobileMenuLists),
+        _step;
+      try {
+        for (_iterator.s(); !(_step = _iterator.n()).done;) {
+          var list = _step.value;
+          list.addEventListener("click", this.toggleMobileGroups.bind(this));
+        }
+      } catch (err) {
+        _iterator.e(err);
+      } finally {
+        _iterator.f();
+      }
+      var _iterator2 = _createForOfIteratorHelper(this.videoLockups),
+        _step2;
+      try {
+        for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {
+          var lockup = _step2.value;
+          lockup.addEventListener("click", this.toggleVideoCopy.bind(this));
+        }
+      } catch (err) {
+        _iterator2.e(err);
+      } finally {
+        _iterator2.f();
+      }
     }
   }, {
     key: "turnOffLoader",
     value: function turnOffLoader() {
       this.loader.parentNode.removeChild(this.loader);
-      this.content.style.display = 'block';
+      this.content.style.display = "block";
       for (var i = 0; i < this.blobs.length; i++) {
-        this.blobs[i].style.opacity = '0.55';
+        this.blobs[i].style.opacity = "0.55";
       }
       if (this.footer) {
-        this.footer.classList.add('show');
+        this.footer.classList.add("show");
       }
     }
   }, {
     key: "toggleMenu",
     value: function toggleMenu() {
-      this.body.classList.toggle('no-scroll');
-      this.mobileMenuBtn.classList.toggle('active');
-      this.mobileMenu.classList.toggle('active');
+      this.body.classList.toggle("no-scroll");
+      this.mobileMenuBtn.classList.toggle("active");
+      this.mobileMenu.classList.toggle("active");
+    }
+  }, {
+    key: "toggleMobileGroups",
+    value: function toggleMobileGroups(e) {
+      if (e.target.parentNode.classList.contains("active")) {
+        e.target.parentNode.classList.remove("active");
+      } else {
+        e.target.parentNode.classList.add("active");
+      }
     }
   }, {
     key: "trackScroll",
     value: function trackScroll() {
+      console.log("trackScroll");
       var offset = window.pageYOffset;
-      if (offset > 0) {
-        this.header.classList.add('scroll');
+      if (offset > 150) {
+        this.header.classList.add("scroll");
       } else {
-        this.header.classList.remove('scroll');
+        this.header.classList.remove("scroll");
       }
+    }
+  }, {
+    key: "toggleVideoCopy",
+    value: function toggleVideoCopy(e) {
+      var id = e.target.getAttribute("data-id");
+      this.videoCopys.forEach(function (copy, index) {
+        if (copy.getAttribute("data-id") == id) {
+          copy.classList.add("hidden");
+        }
+      });
     }
   }, {
     key: "mouseListener",
     value: function mouseListener(e) {
       var xFactor = e.clientX * 0.05;
-      this.blobs[0].style.transform = 'translate3d(' + parseInt(xFactor * 0.4) + 'px, ' + parseInt(xFactor * -1.5) + 'px, 0)';
-      this.blobs[1].style.transform = 'translate3d(' + parseInt(xFactor * -2) + 'px, ' + parseInt(xFactor * -1.5) + 'px, 0)';
-      this.blobs[2].style.transform = 'translate3d(' + parseInt(xFactor * -0.4) + 'px, ' + parseInt(xFactor) + 'px, 0)';
-      this.blobs[3].style.transform = 'translate3d(' + parseInt(xFactor * -0.6) + 'px, ' + parseInt(xFactor * 0.3) + 'px, 0)';
-      this.blobs[4].style.transform = 'translate3d(' + parseInt(xFactor * 1.6) + 'px, ' + parseInt(xFactor * -1.2) + 'px, 0)';
-      this.blobs[5].style.transform = 'translate3d(' + parseInt(xFactor * 2) + 'px, ' + parseInt(xFactor * -1) + 'px, 0)';
-      this.blobs[6].style.transform = 'translate3d(' + parseInt(xFactor * 1.2) + 'px, ' + parseInt(xFactor * 0.5) + 'px, 0)';
+      this.blobs[0].style.transform = "translate3d(" + parseInt(xFactor * 0.4) + "px, " + parseInt(xFactor * -1.5) + "px, 0)";
+      this.blobs[1].style.transform = "translate3d(" + parseInt(xFactor * -2) + "px, " + parseInt(xFactor * -1.5) + "px, 0)";
+      this.blobs[2].style.transform = "translate3d(" + parseInt(xFactor * -0.4) + "px, " + parseInt(xFactor) + "px, 0)";
+      this.blobs[3].style.transform = "translate3d(" + parseInt(xFactor * -0.6) + "px, " + parseInt(xFactor * 0.3) + "px, 0)";
+      this.blobs[4].style.transform = "translate3d(" + parseInt(xFactor * 1.6) + "px, " + parseInt(xFactor * -1.2) + "px, 0)";
+      this.blobs[5].style.transform = "translate3d(" + parseInt(xFactor * 2) + "px, " + parseInt(xFactor * -1) + "px, 0)";
+      this.blobs[6].style.transform = "translate3d(" + parseInt(xFactor * 1.2) + "px, " + parseInt(xFactor * 0.5) + "px, 0)";
     }
   }]);
   return App;
@@ -94,10 +150,6 @@ new App();
 __webpack_require__.r(__webpack_exports__);
 // extracted by mini-css-extract-plugin
 
-<<<<<<< HEAD
-=======
-eval("class App {\n\n\tconstructor() {\n\t\tthis.header = document.querySelector('.header');\n    this.body = document.querySelector('body');\n\n\t\tthis.blobs = document.querySelectorAll('.blob');\n\n\t\tthis.nav = document.querySelector('.header-right__nav');\n\n\t\tthis.mobileMenu = document.querySelector('.header-mobile');\n\t\tthis.mobileMenuBtn = document.querySelector('.header-mobile__button');\n\n\t\tthis.mobileMenuLists = document.querySelectorAll('.header-mobile__nav .nav-link.primary');\n\t\tthis.mobileMenuGroups = document.querySelectorAll('.header-mobile__nav .nav-link-group');\n\n    this.content = document.querySelector('.content-wrapper');\n\n\t\tthis.videoLockups = document.querySelectorAll('.content-lockup.video')\n\t\tthis.videoCopys = document.querySelectorAll('.content-lockup__copy')\n\n\t\tthis.footer = document.querySelector('.footer-wrapper')\n\t\tthis.loader = document.querySelector('.loader');\n\n\t\tthis.bindGlobalEvents();\n\t}\n\n\tbindGlobalEvents() {\n\t\twindow.addEventListener('load', this.turnOffLoader.bind(this));\n\n\t\tif (window.innerWidth > 850) {\n\t\t\tif (window.location.pathname == '/') {\n\t\t\t\tthis.content.style.marginTop = this.header.offsetHeight - 1 + \"px\";\n\t\t\t} else {\n\t\t\t\tthis.content.style.marginTop = this.header.offsetHeight + 70 + \"px\";\n\t\t\t}\n\n\t\t\twindow.addEventListener('scroll', this.trackScroll.bind(this));\n\t\t\tdocument.addEventListener('mousemove', this.mouseListener.bind(this));\n\t\t}\n\n\t\tthis.mobileMenuBtn.addEventListener('click', this.toggleMenu.bind(this));\n\n\t\tfor (const list of this.mobileMenuLists) {\n\t\t\tlist.addEventListener('click', this.toggleMobileGroups.bind(this));\n\t\t}\n\n\t\tfor (const lockup of this.videoLockups) {\n\t\t\tlockup.addEventListener('click', this.toggleVideoCopy.bind(this));\n\t\t}\n\t}\n\n\tturnOffLoader() {\n\t\tthis.loader.parentNode.removeChild(this.loader);\n\t\tthis.content.style.display = 'block';\n\n\t\tfor (let i = 0; i < this.blobs.length; i++) {\n\t\t\tthis.blobs[i].style.opacity = '0.55';\n\t\t}\n\n\t\tif (this.footer) {\n\t\t\tthis.footer.classList.add('show');\n\t\t}\n\t}\n\n\ttoggleMenu() {\n    this.body.classList.toggle('no-scroll');\n    this.mobileMenuBtn.classList.toggle('active');\n    this.mobileMenu.classList.toggle('active');\n  }\n\n\ttoggleMobileGroups(e) {\n\t\tif (e.target.parentNode.classList.contains('active')) {\n\t\t\te.target.parentNode.classList.remove('active');\n\t\t} else {\n\t\t\te.target.parentNode.classList.add('active');\n\t\t}\n\t}\n\n\ttrackScroll() {\n\t\tlet offset = window.pageYOffset;\n\n\t\tif (offset > 150) {\n\t\t\tthis.header.classList.add('scroll');\n\t\t} else {\n\t\t\tthis.header.classList.remove('scroll')\n\t\t}\n\t}\n\n\ttoggleVideoCopy(e) {\n\t\tconst id = e.target.getAttribute('data-id');\n\n\t\tthis.videoCopys.forEach(function (copy, index) {\n\t\t\tif (copy.getAttribute('data-id') == id) {\n\t\t\t\tcopy.classList.add('hidden');\n\t\t\t}\n\t\t});\n\t}\n\n\tmouseListener(e) {\n\t\tlet xFactor = e.clientX * 0.05;\n\n\t\tthis.blobs[0].style.transform = 'translate3d(' + parseInt(xFactor * 0.4) + 'px, ' + parseInt(xFactor * -1.5) + 'px, 0)';\n\t\tthis.blobs[1].style.transform = 'translate3d(' + parseInt(xFactor * -2) + 'px, ' + parseInt(xFactor * -1.5) + 'px, 0)';\n\n\t\tthis.blobs[2].style.transform = 'translate3d(' + parseInt(xFactor * -0.4) + 'px, ' + parseInt(xFactor) + 'px, 0)';\n\t\tthis.blobs[3].style.transform = 'translate3d(' + parseInt(xFactor * -0.6) + 'px, ' + parseInt(xFactor * 0.3) + 'px, 0)';\n\n\t\tthis.blobs[4].style.transform = 'translate3d(' + parseInt(xFactor * 1.6) + 'px, ' + parseInt(xFactor * -1.2) + 'px, 0)';\n\t\tthis.blobs[5].style.transform = 'translate3d(' + parseInt(xFactor * 2) + 'px, ' + parseInt(xFactor * -1) + 'px, 0)';\n\n\t\tthis.blobs[6].style.transform = 'translate3d(' + parseInt(xFactor * 1.2) + 'px, ' + parseInt(xFactor * 0.5) + 'px, 0)';\n\t}\n}\n\nnew App();\n\n\n//# sourceURL=webpack:///./src/scripts/main.js?");
->>>>>>> staging
 
 /***/ })
 
